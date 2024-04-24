aef3aa3ad783a9f3adb5a8c10e3159da34603d4f
Vovaadidas777 <mateberidze464@gmail.com>
2024-04-24 18:28:35 +0400

    uptade commit

--- day51/index.js
+++ day51/index.js
@@ -9,4 +9,29 @@ console.log(myName, myCity, myLastname, mySchool);
 
 
 
+//second hw
+let mother = {
+    firstName: "Natalia",
+    lastName: "Kharchenko",
+    age: 35
+};
+
+let father = {
+    firstName: "Nugzar",
+    lastName: "Beridze",
+    age: 37
+};
+
+console.log("Mother:");
+console.log("First Name:", mother.firstName);
+console.log("Last Name:", mother.lastName);
+console.log("Age:", mother.age);
+
+console.log("\nFather:");
+console.log("First Name:", father.firstName);
+console.log("Last Name:", father.lastName);
+console.log("Age:", father.age);
+
+
+
 
