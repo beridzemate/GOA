e6231e4e279569069a0cc141301f18b70c8f3258
Vovaadidas777 <mateberidze464@gmail.com>
2024-04-24 18:40:59 +0400

    uptade commit

--- day52/script.js
+++ day52/script.js
@@ -12,3 +12,19 @@ let hobby = "coding and learning programming";
 
 let info = infoJoin(firstName, lastName, age, residence, hobby);
 console.log(info);
+//second hw
+
+
+function ageCheck(age) {
+    if (age > 18) {
+        console.log(`${age} is greater than 18.`);
+    } else if (age < 18) {
+        console.log(`${age} is less than 18.`);
+    } else {
+        console.log(`${age} is equal to 18.`);
+    }
+}
+
+ageCheck(20);
+ageCheck(15);
+ageCheck(18);
